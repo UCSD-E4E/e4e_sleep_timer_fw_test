10bd3eb579a783a9a6ed94c487948e47d3620599
Nathan Hui <ntlhui@gmail.com>
2022-05-26 19:29:41 -0700

    Updating launcher and tests

--- tests/test_rtc.py
+++ tests/test_rtc.py
@@ -16,38 +16,52 @@ def test_setTime():
     tester = Tester("COM4")
     with tester:
         output = tester.sendDebugMenuCommand('2')
+        assert(output == b'2\r\n 1: Get RTC Time\r\n 2: Set RTC Time\r\n 3: Hex Clock\r\n 4: Set Alarm\r\n>')
         output = tester.sendDebugMenuCommand('2', ': ')
+        assert(output == b'2\r\nUpper Half Time: ')
         now = int(dt.datetime.now().timestamp() * 1000)
         upper = now >> 32
         lower = now & 0x0FFFFFFFF
         output = tester.sendDebugMenuCommand(f'{hex(upper)}\r', ': ', slow=True)
+        assert(output.endswith(b'\r\nLower Half Time: '))
         output = tester.sendDebugMenuCommand(f'{hex(lower)}\r', slow=True)
+        assert(output.endswith(b'\r\n 2: RTC Debug Menu\r\n>'))
         output = tester.sendDebugMenuCommand('2')
+        # assert(output == b'2\r\n 1: Get RTC Time\r\n 2: Set RTC Time\r\n 3: Hex Clock\r\n 4: Set Alarm\r\n>')
         output = tester.sendDebugMenuCommand('1')
+        assert(output.endswith(b'\r\n 2: RTC Debug Menu\r\n>'))
         st_time = dt.datetime.fromtimestamp(int(output.decode().splitlines()[1].split(': ')[1], 16) / 1000.0)
         assert(abs((st_time - dt.datetime.now()).total_seconds()) < 5)
 
 def test_setAlarm():
     tester = Tester("COM4")
     with tester:
+        # Enter RTC menu
         output = tester.sendDebugMenuCommand('2')
+        assert(output == b'2\r\n 1: Get RTC Time\r\n 2: Set RTC Time\r\n 3: Hex Clock\r\n 4: Set Alarm\r\n>')
         output = tester.sendDebugMenuCommand('2', ': ')
+        assert(output == b'2\r\nUpper Half Time: ')
         now = int(dt.datetime.now().timestamp() * 1000)
         upper = now >> 32
         lower = now & 0x0FFFFFFFF
         output = tester.sendDebugMenuCommand(f'{hex(upper)}\r', ': ', slow=True)
+        assert(output.endswith(b'\r\nLower Half Time: '))
         output = tester.sendDebugMenuCommand(f'{hex(lower)}\r', slow=True)
+        assert(output.endswith(b'\r\n 2: RTC Debug Menu\r\n>'))
 
         alarmTime = dt.datetime.now() + dt.timedelta(seconds=10)
 
         upper = int(alarmTime.timestamp() * 1000) >> 32
         lower = int(alarmTime.timestamp() * 1000) & 0x0FFFFFFFF
 
-        tester.sendDebugMenuCommand('2')
-        tester.sendDebugMenuCommand('4', ': ')
-        tester.sendDebugMenuCommand(f'{hex(upper)}\r', ': ', slow=True)
+        output = tester.sendDebugMenuCommand('2')
+        assert(output == b'2\r\n 1: Get RTC Time\r\n 2: Set RTC Time\r\n 3: Hex Clock\r\n 4: Set Alarm\r\n>')
+        output = tester.sendDebugMenuCommand('4', ': ')
+        assert(output == b'4\r\nUpper Half Alarm: ')        
+        output = tester.sendDebugMenuCommand(f'{hex(upper)}\r', ': ', slow=True)
+        assert(output.endswith(b'\r\nLower Half Alarm: '))
         output = tester.sendDebugMenuCommand(f'{hex(lower)}\r', timeout=30, slow=True)
-        print(output)
+        assert(output.endswith(b'\r\nFlag is now 1\r\n 2: RTC Debug Menu\r\n>'))
 
 if __name__ == '__main__':
     test_setAlarm()
